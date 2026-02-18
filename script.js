dc4da9ed802878a5092e97d8c94430f069bc7734
tanvir-5735 <anjumtanvir5735@gmail.com>
2026-02-18 22:01:46 +0600

    indication

--- script.js
+++ script.js
@@ -1,85 +1,77 @@
-// container id="cvBuilder"
-// for all step class="step"
-// step1 id="step1"
-//personal info  id="profileImage", id="fullName", id="email", id="phone", id="address"
-// step2 id="step2"
-//education info id="schoolName", id="degree", id="fieldOfStudy", id="graduationYear"
-// step3 id="step3"
-//work experience id="companyName", id="jobTitle", id="startDate", id="endDate", id="jobDescription"
-// step4 id="step4"
-//skills and projects id="skills", id="liveprojects" ,id="objective",id="activities"
-// step5 id="step5"
-//review and download button id="downloadBtn"   
+document.addEventListener("DOMContentLoaded" , function (){
 
-document.addEventListener("DOMContentLoaded"), function () {
+    const steps= document.querySelectorAll(".step");
 
-    const steps = document.querySelectorAll(".step");
-    const indicators=document.querySelectorAll(".step-indicator");
+    const indicators = document.querySelectorAll(".step-indicator");
+
     let currentStep=0;
 
-    const cvData={
-        personal:{},
-        profileImage:"",
-        education:[],
-        experience:[],
-        skills:[],
-        objective:"",
-        activities:"",
-        liveProject:"",
-    };
+    // function showStep (jamonchai) {
+    //     steps.forEach((v,i) => {
+    //         steps.classList.remoove("active");
+    //         indicators[i].classList.remove("active");
 
-    function showStep(index) {
-        for (let i=0; i< steps.length; i++)  {
+    //         if(i===jamonchai) {
+    //             steps.classList.add("active");
+    //             indicators[i].classList.add("active");
+    //         }
+    //     });
+    // }
+
+
+     function showStep (jamonchai) {
+
+        for( let i=0 ; i< steps.length ; i++) {
 
             steps[i].classList.remove("active");
             indicators[i].classList.remove("active");
-        } 
+        }
+        steps[jamonchai].classList.add("active");
+        indicators[jamonchai].classList.add("active");
 
-        steps[index].classList.add("active");
-        indicators[index].classList.add("active");
-        window.scrollTo(0, 0);
+     }
+
+       //-----step-1------
+     document.getElementById("next1").onclick=function() {
+        currentStep=1;
+        showStep(currentStep);
+
+     }
+     document.getElementById("prev2").onclick=function() {
+        currentStep=0;
+        showStep(currentStep);
+     }
+        //-----step-2------
+
+     document.getElementById("next2").onclick=function(){
+        currentStep=2;
+        showStep(currentStep);
+     }
+     document.getElementById("prev3").onclick=function() {
+        currentStep=1;
+        showStep(currentStep);
+     }
+        //------step-3------
+
+        document.getElementById("next3").onclick=function(){
+            currentStep=3;
+            showStep(currentStep);
         }
 
-    // function showToast(msg) {
-    //     const container=document.getElementById("toastContainer");
-    //     const toast=document.createElement("div");
-    //     toast.className="toast";
-    //     toast.innerText=msg;
-    //     container.appendChild(toast);
+        document.getElementById("prev4").onclick=function() {
+            currentStep=2;
+            showStep(currentStep);
+        }
+            //------step-4------
+        document.getElementById("next4").onclick=function() {
+            currentStep=4;
+            showStep(currentStep);
+        }
 
-    //     setTimeout(() => toast.classList.add("show"), 10);
-    //     setTimeout(() => {
-    //         toast.classList.remove("show");
-    //         setTimeout(() => toast.remove(), 300);
-    //     },3000);
-    // }
+        document.getElementById("prev5").onclick=function() {
+            currentStep=3;
+            showStep(currentStep);
+        }
 
-    function showToast(msg) {
-        const container= document.getElementById("toastContainer");
-        const toast= document.createElement("div");
-        toast.className="toast";
-        toast.innerTeaxt=msg;
-        container.appendChild(toast);
-
-        setTimeout(() => toast.classList.add("show"), 10);
-        setTimeout(() =>{ toast.classList.remove("show");
-        setTimeout(() => toast.remove(), 300);
-    }, 3000);
-    }
-
-    //profile image upload
-
-    document.getElementById("profileimage").addEventListener("change", function (e) {  //change is a input event 
-        const file = e.target.files[0];
-        if (file) {
-            const reader= new FileReader();
-            reader.onload =function (evt) {
-                cvData.profileImage=evt.target.result;
-            };
-            reader.readAsDataURL(file);
-            }
-        });
-
-    
-
-    }
+    showStep(0);
+});
